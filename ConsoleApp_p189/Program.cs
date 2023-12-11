8d616492e872e14b38276d1639c6adc2792d4e83
sehui <sehuiii0131@naver.com>
2023-12-11 22:14:07 +0900

    Add files via upload

---
+++ ConsoleApp_p189/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_p189
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 1;
+
+            bool state = true;
+            while (state)
+            {
+                Console.Clear();
+                ConsoleKeyInfo info = Console.ReadKey();
+                switch (info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("^_@");
+                        x++;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine();
+                        break;
+                    case ConsoleKey.X:
+                        state = false;
+                        break;
+               }
+            }
+        }
+    }
+}
