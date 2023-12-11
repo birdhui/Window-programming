856909f1a92fcb8deaf2b03041cc29dbe8ba6b86
sehui <sehuiii0131@naver.com>
2023-12-12 04:44:25 +0900

    C# programming basics

---
+++ ConsoleApp_exam0211/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("인치를 입력하시오");
+
+string sInch;
+float nInch;
+sInch = Console.ReadLine();
+bool result = float.TryParse(sInch, out nInch);
+if (result)
+{
+    nInch = float.Parse(sInch);
+    Console.WriteLine(nInch + "inch = " + nInch * 2.54 + "cm");
+}
+else
+    Console.WriteLine(sInch + "는 숫자가 아닙니다");
---
+++ ConsoleApp_exam0212/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Kg을 입력하시오");
+
+string sKg;
+float nKg = 0;
+sKg = Console.ReadLine();
+bool result = float.TryParse(sKg, out nKg);
+if (result)
+{
+    nKg = float.Parse(sKg);
+    Console.WriteLine(nKg + "Kg = " + nKg * 2.20462262 + "pound");
+}
+else
+    Console.WriteLine(sKg + "는 숫자가 아닙니다");
