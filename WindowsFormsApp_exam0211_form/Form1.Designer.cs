fbd9d54e33ea0ea841149b3e3f15da380b77962b
sehui <sehuiii0131@naver.com>
2023-12-11 22:16:32 +0900

    WindowsForms basics

---
+++ WindowsFormsApp_exam0211_form/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp_exam0211_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InchBox = new System.Windows.Forms.TextBox();
+            this.inch2cm = new System.Windows.Forms.Button();
+            this.cmLabel = new System.Windows.Forms.Label();
+            this.KgBox = new System.Windows.Forms.TextBox();
+            this.kg2pound = new System.Windows.Forms.Button();
+            this.poundLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InchBox
+            // 
+            this.InchBox.Location = new System.Drawing.Point(174, 135);
+            this.InchBox.Name = "InchBox";
+            this.InchBox.Size = new System.Drawing.Size(100, 21);
+            this.InchBox.TabIndex = 0;
+            this.InchBox.Text = "0";
+            this.InchBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.InchBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.InchBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InchBox_KeyPress);
+            // 
+            // inch2cm
+            // 
+            this.inch2cm.Location = new System.Drawing.Point(290, 135);
+            this.inch2cm.Name = "inch2cm";
+            this.inch2cm.Size = new System.Drawing.Size(75, 23);
+            this.inch2cm.TabIndex = 1;
+            this.inch2cm.Text = "inch2cm";
+            this.inch2cm.UseVisualStyleBackColor = true;
+            this.inch2cm.Click += new System.EventHandler(this.inch2cm_Click);
+            // 
+            // cmLabel
+            // 
+            this.cmLabel.AutoSize = true;
+            this.cmLabel.Location = new System.Drawing.Point(384, 138);
+            this.cmLabel.Name = "cmLabel";
+            this.cmLabel.Size = new System.Drawing.Size(11, 12);
+            this.cmLabel.TabIndex = 2;
+            this.cmLabel.Text = "0";
+            // 
+            // KgBox
+            // 
+            this.KgBox.Location = new System.Drawing.Point(174, 179);
+            this.KgBox.Name = "KgBox";
+            this.KgBox.Size = new System.Drawing.Size(100, 21);
+            this.KgBox.TabIndex = 3;
+            this.KgBox.Text = "0";
+            this.KgBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.KgBox.TextChanged += new System.EventHandler(this.KgBox_TextChanged);
+            this.KgBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KgBox_KeyPress);
+            // 
+            // kg2pound
+            // 
+            this.kg2pound.Location = new System.Drawing.Point(290, 177);
+            this.kg2pound.Name = "kg2pound";
+            this.kg2pound.Size = new System.Drawing.Size(75, 23);
+            this.kg2pound.TabIndex = 4;
+            this.kg2pound.Text = "kg2pound";
+            this.kg2pound.UseVisualStyleBackColor = true;
+            this.kg2pound.Click += new System.EventHandler(this.kg2pound_Click);
+            this.kg2pound.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.kg2pound_KeyPress);
+            // 
+            // poundLabel
+            // 
+            this.poundLabel.AutoSize = true;
+            this.poundLabel.Location = new System.Drawing.Point(384, 182);
+            this.poundLabel.Name = "poundLabel";
+            this.poundLabel.Size = new System.Drawing.Size(11, 12);
+            this.poundLabel.TabIndex = 6;
+            this.poundLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.poundLabel);
+            this.Controls.Add(this.kg2pound);
+            this.Controls.Add(this.KgBox);
+            this.Controls.Add(this.cmLabel);
+            this.Controls.Add(this.inch2cm);
+            this.Controls.Add(this.InchBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InchBox;
+        private System.Windows.Forms.Button inch2cm;
+        private System.Windows.Forms.Label cmLabel;
+        private System.Windows.Forms.TextBox KgBox;
+        private System.Windows.Forms.Button kg2pound;
+        private System.Windows.Forms.Label poundLabel;
+    }
+}
+
---
+++ WindowsFormsApp_exam0211_form/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_exam0211_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inch2cm_Click(object sender, EventArgs e)
+        // 버튼 클릭 이벤트 - 이벤트 발생시 InchBox에 입력한 값을 가져옴
+        {
+            // 소수점 개수가 1이거나 이하이고, InchBox.Text값이 부동 소수점 숫자라면
+            if (InchdecimalCount <= 1 && float.TryParse(InchBox.Text, out float val))
+            {
+                // 값을 cm로 변환 후 'cmLabel' 텍스트를 변환된 값으로 설정, 인치 값을 2.54 곱함
+                cmLabel.Text = (val * 2.54).ToString();
+                InchdecimalCount = 0; // 변환 후 소수점 개수 초기화
+            }
+            else
+                // 부동 소수점 숫자가 아닐 시 'cmLabel'의 텍스트 ERROR 표시
+                cmLabel.Text = "ERROR";
+        }
+
+        // 소수점 개수 저장
+        private int InchdecimalCount = 0;
+        private void InchBox_KeyPress(object sender, KeyPressEventArgs e)
+        // 텍스트 상자에서 키를 누를 때 발생하는 키 누름 이벤트 처리 메소드
+        {
+            // 입력된 키가 소수점 키라면
+            if (e.KeyChar == Convert.ToChar(46))
+            {
+                // 소수점 키가 이미 입력된 경우
+                if (InchdecimalCount > 0)
+                {
+                    // 키 입력 무시
+                    e.Handled = true;
+                }
+                // 아니라면
+                else
+                {
+                    // 소수점 개수 증가
+                    InchdecimalCount++;
+                }
+            }
+            // 숫자, 백스페이스 및 소수점이 아닌 경우 입력 무시
+            else if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void kg2pound_Click(object sender, EventArgs e)
+        {
+            // 소수점 개수가 1이거나 이하이고, InchBox.Text값이 부동 소수점 숫자라면
+            if (kgdecimalCount <= 1 && float.TryParse(KgBox.Text, out float val))
+            {
+                // 값을 cm로 변환 후 'cmLabel' 텍스트를 변환된 값으로 설정, 인치 값을 2.54 곱함
+                poundLabel.Text = (float.Parse(KgBox.Text) * 2.20462262).ToString();
+                kgdecimalCount = 0; // 변환 후 소수점 개수 초기화
+            }
+            else
+                // 부동 소수점 숫자가 아닐 시 'cmLabel'의 텍스트 ERROR 표시
+                poundLabel.Text = "ERROR";
+        }
+
+        private int kgdecimalCount = 0;
+        private void KgBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 입력된 키가 소수점 키라면
+            if (e.KeyChar == Convert.ToChar(46))
+            {
+                // 소수점 키가 이미 입력된 경우
+                if (kgdecimalCount > 0)
+                {
+                    // 키 입력 무시
+                    e.Handled = true;
+                }
+                // 아니라면
+                else
+                {
+                    // 소수점 개수 증가
+                    kgdecimalCount++;
+                }
+            }
+            // 숫자, 백스페이스 및 소수점이 아닌 경우 입력 무시
+            else if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void KgBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kg2pound_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
